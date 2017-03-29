866ee3fb13e005609c076c67f4b9e9c207d93f17
RadoslavGL <radoslavlgeorgiev@gmail.com>
2017-03-29 14:08:46 +0300

    comments added to Binary Search

--- Arrays/Arrays-DemoAndHW/Binary search/BinarySearch.cs
+++ Arrays/Arrays-DemoAndHW/Binary search/BinarySearch.cs
@@ -14,12 +14,25 @@
                 arrN[i] = int.Parse(Console.ReadLine());
             }
 
+            //read the searched number from the console
+            //create bool variable that checks if the index was found
             int xValue = int.Parse(Console.ReadLine());
             bool isInTheArray = false;
 
+            //all be below listed are not values - they are indexes for the elements in the array;
+            //create lower and upper borders for the search - lower = 0; upper = array.Length - 1;
+            //create middle of the search to compare to; middle = (upper + lower) / 2 and this is logical;
             int lowerBorder = 0;
             int upperBorder = arrN.Length - 1;
             int middle = (upperBorder + lowerBorder) / 2;
+
+            //search for the xValue and its index; 
+            //logic: compare the array element with index "middle" to xValue and manage the borders if not matched;
+            //if arrN[middle] < xValue -> lower border increased to middle + 1; 1 above the middle;
+            //if arrN[middle] > xValue -> upper border decreased to middle - 1; 1 below the middle;
+            //if arrN[middle] == xValue -> bool == true and the while condition stops
+            //if lower >= upper and arrN[middle] != xValue we are still stopping as this means that value we search for is not in array
+            // middle is recalculation for each iteration;
 
             while (upperBorder >= lowerBorder && isInTheArray != true)
             {
@@ -39,6 +52,9 @@
                 }
 
             }
+
+            //if bool == true -> we found the value in the array e.i. arrN[middle] == middle;
+            //if bool == false -> we have not found the value and as per the instructions, we print "-1"
 
             if (isInTheArray == true)
             {
